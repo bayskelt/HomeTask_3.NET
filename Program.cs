f4523226bb048c30fa8d0fe5403a97ca446d0a8e
bayskelt <rinognom@gmail.com>
2024-03-26 22:33:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeTask_3.NET
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введiть цiле число A: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Введiть цiле число B (воно має бути меншим, нiж A): ");
+            int B = int.Parse(Console.ReadLine());
+
+            if (A > B)
+            {
+                do
+                {
+                    Console.WriteLine(A);
+                    A--;
+                }
+                while (A >= B);
+            }
+            else 
+            {
+                Console.Write("\nВи ввели неправильнi вхiднi данi! (число A менше за B)");
+            }
+        }
+    }
+}
